f18d070003fe97849efa80e9ef263e4c0c1448d7
Vladislav Salnikov <119621943+enjusilence@users.noreply.github.com>
2023-04-30 20:47:12 +0300

    Добавит комментарии

--- Frontend/react-app/src/components/my-tests-page-content/my-tests-page-content.jsx
+++ Frontend/react-app/src/components/my-tests-page-content/my-tests-page-content.jsx
@@ -13,6 +13,7 @@ export const MyTestsPageContent = () => {
   let nextPage = useRef();
   let isLoading = useRef(false);
 
+  // Список ссылок в подвале плашке
   const linkList = (id) => ([
     {key: 1, link: `${AppRoute.EditTestDescription}/${id}`, label: 'Описание'},
     {key: 2, link: `${AppRoute.EditTest}/${id}`, label: 'Редактировать'},
--- Frontend/react-app/src/components/profile-tests/profile-tests.jsx
+++ Frontend/react-app/src/components/profile-tests/profile-tests.jsx
@@ -1,14 +1,18 @@
 import styles from './profile-tests.module.scss';
 import { ProfileNavigation } from '../profile-navigation/profile-navigation';
 import { PassedTestComponent } from './passed-test-component/passed-test-component'
+import { TestListProfile } from '../test-list-profile/test-list-profile';
+import { useState } from 'react';
 
 export const ProfileTestsComponent = () => {
+    // useState
     return (
         <>
             <main className={styles.pageMain}>
             <ProfileNavigation />
             <div className={styles.contentForm} action="#" name="temp-name">
                 <h1 className={styles.createTest}>Пройденные</h1>
+                {/* <TestListProfile /> */}
                 <PassedTestComponent />
                 <PassedTestComponent />
                 <PassedTestComponent />
