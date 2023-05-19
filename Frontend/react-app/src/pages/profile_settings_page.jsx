ac2a5e2bd47f5b0d48c5e553b96946d9031e23b6
K-Isa <iskr2200@gmail.com>
2023-05-19 18:46:14 +0300

    добавит страницу profile-setting-page и изменит компонент страницы reset-password-change

--- Frontend/react-app/src/pages/profile_settings_page.jsx
+++ Frontend/react-app/src/pages/profile_settings_page.jsx
@@ -1,11 +1,15 @@
 import { Footer } from '../components/footer/footer';
+import { useParams } from 'react-router';
 import { Header } from '../components/header/header';
+import { ProfileSettingComponent } from '../components/profile-setting-page/profile-setting-page';
 
 export const ProfileSettingPage = () => {
+    const {testID} = useParams();
     
     return (
         <>
             <Header />
+            <ProfileSettingComponent testID={testID} />
             <Footer />
         </>
     );
