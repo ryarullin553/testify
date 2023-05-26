c8db0704476a4d54b1be301dd203909de7749182
Ruslan Yarullin <ryarullin553@gmail.com>
2023-05-26 12:53:13 +0300

    Добавит удаление закладки по id теста

--- djangoProject/user_relations/views.py
+++ djangoProject/user_relations/views.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from rest_framework import viewsets, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
@@ -21,6 +20,22 @@ class BookmarkAPIView(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewset
         page = self.paginate_queryset(queryset)
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        test_id = self.kwargs[lookup_url_kwarg]
+        obj = get_object_or_404(queryset, test=test_id, user=self.request.user)
+        self.check_object_permissions(self.request, obj)
+        return obj
 
 
 class FeedbackAPIView(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
