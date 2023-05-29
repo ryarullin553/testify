bf9d62e0e5273d356afee525c99a4e5d836b8a8e
Ruslan Yarullin <ryarullin553@gmail.com>
2023-05-29 10:01:49 +0300

    Обновит API для профиля пользователя

--- djangoProject/users/views.py
+++ djangoProject/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import User
@@ -14,10 +15,15 @@ class UserAPIView(mixins.UpdateModelMixin, viewsets.GenericViewSet):
     def retrieve(self, request, *args, **kwargs):
         """Возвращает профиль пользователя"""
         user = self.get_object()
-        fields = ['username', 'avatar', 'bio', 'finished_tests']
-        if user == request.user:
-            serializer = self.get_serializer(user, fields=fields + ['unfinished_tests'])
-        else:
-            serializer = self.get_serializer(user, fields=fields + ['created_tests'])
+        fields = ['username', 'avatar', 'bio', 'finished_tests', 'created_tests']
+        serializer = self.get_serializer(user, fields=fields)
         return Response(serializer.data)
+
+    @action(detail=False, url_path='me', url_name='me')
+    def get_current_user_profile(self, request):
+        """Возвращает профиль текущего пользователя"""
+        fields = ['username', 'avatar', 'bio', 'finished_tests', 'unfinished_tests']
+        serializer = self.get_serializer(request.user, fields=fields)
+        return Response(serializer.data)
+
 
