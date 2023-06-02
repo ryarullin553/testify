41d4eebb62cfe24a3af40fc490e7735bd7612759
Vladislav Salnikov <119621943+enjusilence@users.noreply.github.com>
2023-06-02 18:32:39 +0300

    Починит отображение страниц пользователей

--- Frontend/react-app/src/components/profile-page/profile-component/profile-component.jsx
+++ Frontend/react-app/src/components/profile-page/profile-component/profile-component.jsx
@@ -1,40 +1,46 @@
 import styles from './profile-component.module.scss';
 import { TestListProfile } from '../../test-list-profile/test-list-profile';
-import { ShowMoreButton } from '../show-more-button/show-more-button';
 import { AvatarBlock } from '../../avatar-block/avatar-block';
 import { AppRoute } from '../../../reusable/const';
 
 export const ProfileComponent = ({userInfo}) => {
 
-    const linkListUnfinished = (id) => ([
-        { key: 1, link: `${AppRoute.TestDescription}/${id}`, label: 'Описание' },
-    ]);
 
-    const linkListFinished = (id) => ([
-        { key: 1, link: `${AppRoute.TestDescription}/${id}`, label: 'Описание' },
-        { key: 2, link: '#', label: 'Результаты' },
-    ]);
+  const linkListUnfinished = (id) => ([
+    { key: 1, link: `${AppRoute.TestDescription}/${id}`, label: 'Описание' },
+  ]);
 
-    if (!userInfo) return <></>;
+  const linkListFinished = (id) => ([
+    { key: 1, link: `${AppRoute.TestDescription}/${id}`, label: 'Описание' },
+  ]);
 
-    return (
-        <section className={styles.sectionMain}>
-            <section className={styles.tests}>
-                <section className={styles.info}>
-                    <div className={styles.container}>
-                        <AvatarBlock additionalStyle={styles.logo} src={userInfo.avatar} size={99}/>
-                        <div className={styles.content}>
-                            <h3 className={styles.name}>{userInfo.username}</h3>
-                            <p className={styles.about}>{userInfo.bio}</p>
-                        </div>
-                    </div>
-                </section>
-            </section>
-            <h1>Прохожу сейчас</h1>
-            <TestListProfile testList={userInfo.unfinished_tests} linkList={linkListUnfinished} isAttemptsAvailiable />
-            <h1>Пройденные</h1>
-            <TestListProfile testList={userInfo.finished_tests} linkList={linkListFinished} isAttemptsAvailiable />
-            <ShowMoreButton />
+  if (!userInfo) return <></>;
+
+  return (
+    <section className={styles.sectionMain}>
+      <section className={styles.tests}>
+        <section className={styles.info}>
+          <div className={styles.container}>
+            <AvatarBlock additionalStyle={styles.logo} src={userInfo.avatar} size={99}/>
+            <div className={styles.content}>
+              <h3 className={styles.name}>{userInfo.username}</h3>
+              <p className={styles.about}>{userInfo.bio}</p>
+            </div>
+          </div>
         </section>
-    );
+      </section>
+      {
+        !!userInfo.unfinishedTestList &&
+        <><h1>Прохожу сейчас</h1>
+        <TestListProfile testList={userInfo.unfinishedTestList} linkList={linkListUnfinished} isAttemptsAvailiable /></>
+      }
+      {
+        !!userInfo.createdTestList &&
+        <><h1>Созданные тесты</h1>
+        <TestListProfile testList={userInfo.createdTestList} linkList={linkListUnfinished} isAttemptsAvailiable /></>
+      }
+      <h1>Пройденные</h1>
+      <TestListProfile testList={userInfo.finishedTestList} linkList={linkListFinished} isAttemptsAvailiable />
+    </section>
+  );
 }
