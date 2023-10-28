74a218770d34728364f4259cf0c6d723a5383fb6
Ruslan <ryarullin553@gmail.com>
2023-10-28 17:47:12 +0300

    TES-165 Обновит настройки сборки.

--- backend/users/email.py
+++ backend/users/email.py
@@ -30,5 +30,5 @@ class PasswordResetEmail(BaseEmailMessage):
         context["uid"] = utils.encode_uid(user.pk)
         context["token"] = default_token_generator.make_token(user)
         context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
-        context["domain"] = '127.0.0.1:3000'
+        context["domain"] = ALLOWED_HOSTS[0]
         return context
